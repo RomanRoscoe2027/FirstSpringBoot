2cfc3d42c402c4596ddfb6a70aa7974ecf7c26cd
RomanRoscoe2027 <roscoer1@msu.edu>
2026-02-25 11:40:22 -0800

    The beginning of my first cursed java spring project. Created rough draft of Workout class and empty workout test class which will be implemented while working on excercise class.

---
+++ src/main/java/com/example/fitnessapp/Workout.java
@@ -0,0 +1,39 @@
+package com.example.fitnessapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class Workout holds information regarding a specific workout such as:
+ * mId = the indicator of each workout for differentiating workouts
+ * mDayName = the name of the workout in question, could be split, or simply the day
+ * mExcercises = a list that holds the collection of excercises for a workout. Each of object excercise
+ *
+ * Will hopefully delete default constructor one day when I figure out if there is a
+ * point to do so in Java like C++.
+ *
+ * This workout class is still under development.
+ */
+public class Workout
+{
+    private int mId = 0;
+    // all workouts will have in id
+    private String mDayName;
+    // all workouts have a name like push, pull, legs etc
+    private List<Exercise> mExercises;
+    // Have each workout hold a list containing all exercises of object type exercise from exercise class
+
+    public Workout(int id, String name)
+    {
+        this.mId = id;
+        this.mDayName = name;
+        this.mExercises = new ArrayList<Exercise>();
+    }
+
+    public void AddExcercise(Exercise exercise)
+    {
+        mExercises.add(exercise);
+    }
+
+
+}
