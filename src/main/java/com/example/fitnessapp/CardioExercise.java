3f2213995d8b01b5bd2cead84020db10a131899b
RomanRoscoe2027 <roscoer1@msu.edu>
2026-02-27 12:44:15 -0800

    Create cardio and lifting excercises, child class of base exercise. I need to spell that word correctly god.

--- src/main/java/com/example/fitnessapp/CardioExercise.java
+++ src/main/java/com/example/fitnessapp/CardioExercise.java
@@ -1,4 +1,25 @@
 package com.example.fitnessapp;
 
-public class CardioExercise {
+public class CardioExercise extends Exercise {
+
+    private int mDuration;
+    private double mDistance;
+
+
+    public CardioExercise(String name, int reps, int sets, int duration, double distance) {
+        super(name, reps, sets);
+
+        this.mDuration = duration;
+        this.mDistance = distance;
+    }
+
+    public int getmDuration() {
+        return mDuration;
+    }
+
+
+    public double getmDistance() {
+        return mDistance;
+    }
+
 }
--- src/main/java/com/example/fitnessapp/LiftingExercise.java
+++ src/main/java/com/example/fitnessapp/LiftingExercise.java
@@ -1,4 +1,14 @@
 package com.example.fitnessapp;
 
-public class LiftingExercise {
+public class LiftingExercise extends Exercise {
+
+    private int mWeight;
+
+    public LiftingExercise(String name, int reps, int sets, int weight) {
+        super(name, reps, sets);
+    }
+
+    public int getmWeight() {
+        return mWeight;
+    }
 }
